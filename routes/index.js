f4eb87a9afe076f9fb9ccd8ea1c2b1ab020f01ca
Nilton Carlos <niltonfirminofs@gmail.com>
2022-01-26 17:47:05 -0300

    Final

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const Link = require('../models/link');
+
+router.get('/:code', async (req, res, next) => {
+  const code = req.params.code;
+ 
+  const resultado = await Link.findOne({ where: { code } });
+  if (!resultado) return res.sendStatus(404);
+ 
+  await resultado.save();
+ 
+  res.redirect(resultado.url);
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -18,8 +30,13 @@ function generateCode() {
 router.post('/new', async (req, res, next) => {
   const url = req.body.url;
   const code = generateCode();
- 
-  res.send(`${process.env.DOMAIN}${code}`);
+
+  const resultado = await Link.create({
+    url,
+    code
+  })
+  res.render('stats',resultado.dataValues);
 })
+
 
 module.exports = router;
