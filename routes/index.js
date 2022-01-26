dc65cbdb8a054cececd85d6534832e4c2e185148
Nilton Carlos <niltonfirminofs@gmail.com>
2022-01-26 16:58:26 -0300

    DataBase

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Encurtador de URL' });
 });
+
+
+function generateCode() {
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 5; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  return text;
+}
+
+router.post('/new', async (req, res, next) => {
+  const url = req.body.url;
+  const code = generateCode();
+ 
+  res.send(`${process.env.DOMAIN}${code}`);
+})
 
 module.exports = router;
