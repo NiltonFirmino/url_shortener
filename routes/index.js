018e395b111251f989e3090fea9bbae35e1a357a
Nilton Carlos <niltonfirminofs@gmail.com>
2022-01-30 09:28:04 -0300

    deploy + jsdoc

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 const Link = require('../models/link');
 
+
 router.get('/:code', async (req, res, next) => {
   const code = req.params.code;
- 
+  
   const resultado = await Link.findOne({ where: { code } });
   if (!resultado) return res.sendStatus(404);
  
@@ -19,7 +20,10 @@ router.get('/', function(req, res, next) {
 });
 
 
-
+/**
+ * 
+ * @returns {Promise<void>}
+ */
 function generateCode() {
   let text = '';
   const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_ABCDEFGHIJKLMNOPQRSTUVWXYZ';
